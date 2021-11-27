887532acffb4bc50ac39fce4388b47df023aac05
Makiiiiii <makiko-happy1231@ezweb.ne.jp>
2021-11-28 01:10:08 +0900

    week18 quizy\¦

---
+++ posse-3rd-laravel-study/resources/views/quizy/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="{{ asset('/css/quizy.css') }}"
+>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quizy</title>
+      
+</head>
+
+<body> 
+
+    <!-- <div class="quiz">
+        <h3><span class="chimei">1.この地名はなんて読む？</span></h3>
+        <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png" alt="高輪">
+            <ul>
+                <li class='list' id='machigai1'>たかわ</li>
+                <li class='list' id='machigai2'>こうわ</li>
+                <li class='list' id ='seikai'>たかなわ</li>
+            </ul>
+                <div class="torf" id='torf'>
+                    <p id='kekka1'></p>
+                    <p id='kekka2'>正解は「たかなわ」です！</p>
+                </div>
+         
+    </div> -->
+           
+  <script src="{{ asset('/js/quizy.js') }}"></script>
+    
+</body>
+</html>
