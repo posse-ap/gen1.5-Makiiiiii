61217f53dac5c03bbd8d212105832a8cc5e310c1
Makiiiiii <makiko.lucky1231@gmail.com>
2022-10-23 16:04:09 +0900

    webapp資材をおいた

--- posse-3rd-laravel-study/database/seeds/StudylogsTableSeeder.php
+++ posse-3rd-laravel-study/database/seeds/StudylogsTableSeeder.php
@@ -14,37 +14,37 @@ class StudylogsTableSeeder extends Seeder
         DB::table('studylogs')->insert([
             [
                 'user_id' => 1,
-                'date' => '2022-03-01',
+                'date' => '2022-10-01',
                 'study_time' => 2,
                 'comment' => '久しぶりに頑張った',
             ],
             [
                 'user_id' => 1,
-                'date' => '2022-03-02',
+                'date' => '2022-10-02',
                 'study_time' => 3,
                 'comment' => '久しぶりに頑張った',
             ],
             [
                 'user_id' => 1,
-                'date' => '2022-03-03',
+                'date' => '2022-10-03',
                 'study_time' => 2,
                 'comment' => '頑張った',
             ],
             [
                 'user_id' => 1,
-                'date' => '2022-03-04',
+                'date' => '2022-10-04',
                 'study_time' => 6,
                 'comment' => '頑張った',
             ],
             [
                 'user_id' => 1,
-                'date' => '2022-03-05',
+                'date' => '2022-10-05',
                 'study_time' => 5,
                 'comment' => '頑張った',
             ],
             [
                 'user_id' => 1,
-                'date' => '2022-03-14',
+                'date' => '2022-10-14',
                 'study_time' => 3,
                 'comment' => '頑張った',
             ],
